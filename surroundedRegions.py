2fdf4746e367a1898254dac59f6b107642e70208
Eugene <eugmwanmwan@gmail.com>
2026-02-02 21:07:43 +0300

    Added solution to problem 130 Surrounded Regions

---
+++ surroundedRegions.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+  def solve(self, board: List[List[str]]) -> None:
+    """
+    Do not return anything, modify board in-place instead.
+    """
+    m,n = len(board), len(board[0])
+    
+    def dfs(r,c):
+      if r < 0 or r >= m or c < 0 or c >= n or board[r][c] != 'O':
+        return
+      board[r][c] = "Y"
+      dfs(r+1,c)
+      dfs(r-1,c)
+      dfs(r,c+1)
+      dfs(r,c-1)
+    
+    for r in range(m):
+      dfs(r,0)
+      dfs(r,n-1)
+    for c in range(n):
+      dfs(0,c)
+      dfs(m-1,c)
+    for r in range(m):
+      for c in range(n):
+        if board[r][c] == 'O':
+          board[r][c] = 'X'
+        elif board[r][c] == 'Y':
+          board[r][c] = 'O'
