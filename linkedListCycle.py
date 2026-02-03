34a86738bf5563603e90e257f8ebb632a9bb4186
Eugene <eugmwanmwan@gmail.com>
2026-02-03 21:24:51 +0300

    Added solution to problem 141 Linked List Cyle

---
+++ linkedListCycle.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+  def hasCycle(self, head: Optional[ListNode]) -> bool:
+    while head:
+      if head == "#": return True
+      tmp = head
+      head = head.next
+      tmp.next = "#"
+    return False
