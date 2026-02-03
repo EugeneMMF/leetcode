0552e869a552a05bea12ffd97bbb964bed7bb767
Eugene <eugmwanmwan@gmail.com>
2026-02-03 21:31:05 +0300

    Added solution to problem 142 Linked List Cycle II

---
+++ linkedListCycle2.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+  def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    seen = set()
+    while head:
+      if id(head) in seen: return head
+      seen.add(id(head))
+      head = head.next
+    return None
