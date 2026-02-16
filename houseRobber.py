026f2b92c09524ea66653cfc4e07eeea91f4e76c
Eugene <eugmwanmwan@gmail.com>
2026-02-16 18:08:26 +0300

    Added solution to problem 198 House Robber

---
+++ houseRobber.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+  def rob(self, nums: List[int]) -> int:
+    rob1, rob2 = 0, 0
+    for num in nums:
+      rob1, rob2 = rob2, max(num+rob1, rob2)
+    return rob2
+
+print(Solution().rob([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]))
