756d19a9c6f9ecd6d793b0ec8947ab877b51c8df
Eugene <eugmwanmwan@gmail.com>
2026-02-16 17:33:20 +0300

    Added solution to problem 197 Rising Temperature

---
+++ risingTemperature.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select t1.id from Weather t1
+join Weather t2 on date_sub(str_to_date(t1.recordDate, '%Y-%m-%d'), interval 1 day) = t2.recordDate
+where t1.temperature > t2.temperature;
