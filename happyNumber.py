40fcd5fd8564292a4a5dd10bbd8b5a80b692b83f
Eugene <eugmwanmwan@gmail.com>
2026-02-16 19:35:49 +0300

    Added solution to problem 202 Happy Number

---
+++ happyNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+  def isHappy(self, n: int) -> bool:
+    seen = set()
+    while n != 1 and n not in seen:
+      seen.add(n)
+      sum = 0
+      while n != 0:
+        digit = n%10
+        sum += digit**2
+        n //= 10
+      n = sum
+    return n == 1
