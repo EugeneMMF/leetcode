6cc71edde1271d5e06eb044b5cbf3e3786e80c85
Eugene <eugmwanmwan@gmail.com>
2026-02-17 13:53:24 +0300

    Added solution for problem 204 Count PrimesUpdated solution for problem Revers Bits

--- reverseBits.py
+++ reverseBits.py
@@ -1,6 +1,5 @@
 class Solution:
   def reverseBits(self, n: int) -> int:
     bits = bin(n)[2:]
-    bits = "0"*(32-len(bits)) + bits
     bits = bits[::-1]
-    return int(bits, 2)
+    return int(bits, 2) << (32 - len(bits))
