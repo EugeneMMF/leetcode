759d0588012b7196136f0ce6d8b61a849f750c5b
Eugene <eugmwanmwan@gmail.com>
2026-02-17 19:49:11 +0300

    Added solution to problem 209 Minimum Size Subarray Sum

--- minimumSizeSubarraySum.js
+++ minimumSizeSubarraySum.js
@@ -4,17 +4,19 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-  first = 0
-  last = 0
-  sum = 0
-  minimum = nums.length;
+  let first = 0
+  let last = 0
+  let sum = 0
+  let minimum = nums.length;
+  let hit = false
   for (let i=0; i<nums.length; i++) {
     sum += nums[i];
     last = i+1;
     while (sum >= target) {
+      hit = true
       minimum = last-first < minimum ? last-first : minimum;
       sum -= nums[first++];
     }
   }
-  return minimum;
+  return hit ? minimum : 0;
 };
