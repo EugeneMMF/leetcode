b1d11cdbd13568c43b8059bccb93af8b71c84c28
root <eugmwanmwan@gmail.com>
2026-02-27 16:21:44 +0000

    *** Added solution to problem  290 Word Pattern

---
+++ word-pattern.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(' ')
+
+        if len(pattern) != len(words):
+            return False
+
+        pattern_to_word = {}
+        word_to_pattern = {}
+
+        for i in range(len(pattern)):
+            char = pattern[i]
+            word = words[i]
+
+            if char in pattern_to_word:
+                if pattern_to_word[char] != word:
+                    return False
+            else:
+                pattern_to_word[char] = word
+
+            if word in word_to_pattern:
+                if word_to_pattern[word] != char:
+                    return False
+            else:
+                word_to_pattern[word] = char
+        
+        return True
