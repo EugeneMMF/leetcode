21d690f600bd66153611cce1011909a2f9a3c780
root <eugmwanmwan@gmail.com>
2026-03-07 07:01:59 +0000

    *** Added solution to problem  365 Water and Jug Problem

---
+++ water-and-jug-problem.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution:
+    def canMeasureWater(self, x: int, y: int, target: int) -> bool:
+        if target == 0:
+            return True
+        
+        if x + y < target:
+            return False
+        
+        g = math.gcd(x, y)
+        
+        return target % g == 0
