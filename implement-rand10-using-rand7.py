7a188ad1342b82a6bde0e54a1c7525a7c88b8f17
Eugene <eugmwanmwan@gmail.com>
2026-03-20 14:10:04 +0300

    *** Added solution to problem 470 Implement Rand10() Using Rand7()

---
+++ implement-rand10-using-rand7.py
@@ -0,0 +1,10 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        while True:
+            val = (rand7() - 1) * 7 + rand7()
+            if val <= 40:
+                return (val - 1) % 10 + 1
