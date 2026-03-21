a202fd290162a365ec693e8d9f59fa4135820253
root <eugmwanmwan@gmail.com>
2026-03-21 06:04:02 +0000

    *** Added solution to problem  507 Perfect Number

---
+++ perfect-number.py
@@ -0,0 +1,16 @@
+import math
+
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+
+        sum_of_proper_divisors = 1
+
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                sum_of_proper_divisors += i
+                if i * i != num:
+                    sum_of_proper_divisors += num // i
+
+        return sum_of_proper_divisors == num
