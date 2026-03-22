188e0a0b93dff2b1fe441eede2290bbbb583a0cd
root <eugmwanmwan@gmail.com>
2026-03-22 06:03:17 +0000

    *** Added solution to problem  912 Random Pick with Weight

---
+++ random-pick-with-weight.py
@@ -0,0 +1,17 @@
+import random
+import bisect
+
+class Solution:
+
+    def __init__(self, w: list[int]):
+        self.prefix_sums = []
+        current_sum = 0
+        for weight in w:
+            current_sum += weight
+            self.prefix_sums.append(current_sum)
+        self.total_sum = current_sum
+
+    def pickIndex(self) -> int:
+        target = random.randint(1, self.total_sum)
+        index = bisect.bisect_left(self.prefix_sums, target)
+        return index
