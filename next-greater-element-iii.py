895f9bfcfd51c93087b2fbcd130c2cca7b8bc68f
root <eugmwanmwan@gmail.com>
2026-03-23 06:03:00 +0000

    *** Added solution to problem  556 Next Greater Element III

---
+++ next-greater-element-iii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        s = list(str(n))
+        
+        i = len(s) - 2
+        while i >= 0 and s[i] >= s[i+1]:
+            i -= 1
+        
+        if i == -1:
+            return -1
+            
+        j = len(s) - 1
+        while j > i and s[j] <= s[i]:
+            j -= 1
+            
+        s[i], s[j] = s[j], s[i]
+        
+        left = i + 1
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+            
+        result_str = "".join(s)
+        
+        MAX_INT = 2**31 - 1
+        
+        try:
+            result_int = int(result_str)
+        except ValueError:
+            return -1 # Should not happen if s contains only digits
+            
+        if result_int > MAX_INT:
+            return -1
+            
+        return result_int
