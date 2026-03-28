e89c59643851d52507aaa3d959ae9cb3285b20f6
root <eugmwanmwan@gmail.com>
2026-03-28 19:43:35 +0000

    *** Added solution to problem  686 Repeated String Match

--- repeated-string-match.py
+++ repeated-string-match.py
@@ -2,11 +2,20 @@ class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         len_a = len(a)
         len_b = len(b)
-        num_repeats = (len_b + len_a - 1) // len_a
-        s_repeated = a * num_repeats
-        if b in s_repeated:
-            return num_repeats
-        s_repeated_plus_one = a * (num_repeats + 1)
-        if b in s_repeated_plus_one:
-            return num_repeats + 1
+
+        count = (len_b + len_a - 1) // len_a
+
+        repeated_a = a * count
+
+        if b in repeated_a:
+            return count
+
+        repeated_a += a
+        if b in repeated_a:
+            return count + 1
+        
+        repeated_a += a
+        if b in repeated_a:
+            return count + 2
+
         return -1
