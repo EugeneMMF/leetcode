3f2fd529545223db37cfaf24041e366d5a162587
root <eugmwanmwan@gmail.com>
2026-03-29 06:02:51 +0000

    *** Added solution to problem  698 Partition to K Equal Sum Subsets

--- partition-to-k-equal-sum-subsets.py
+++ partition-to-k-equal-sum-subsets.py
@@ -1,45 +1,48 @@
+import functools
+
 class Solution:
     def canPartitionKSubsets(self, nums: list[int], k: int) -> bool:
-        n = len(nums)
         total_sum = sum(nums)
-
+        
         if total_sum % k != 0:
             return False
-
-        target_sum = total_sum // k
-
+        
+        target_sum_per_subset = total_sum // k
+        
         nums.sort(reverse=True)
-
-        if nums[0] > target_sum:
+        
+        if nums[0] > target_sum_per_subset:
             return False
 
+        buckets = [0] * k
+        
         memo = {}
 
-        def solve(bucket_idx, current_sum, used_mask):
-            if bucket_idx == k:
+        def backtrack(index: int) -> bool:
+            if index == len(nums):
                 return True
-
-            if current_sum == target_sum:
-                return solve(bucket_idx + 1, 0, used_mask)
-
-            if (used_mask, current_sum) in memo:
-                return memo[(used_mask, current_sum)]
-
-            for i in range(n):
-                if (used_mask >> i) & 1:
+            
+            canonical_buckets_state = tuple(sorted(buckets))
+            
+            if (index, canonical_buckets_state) in memo:
+                return memo[(index, canonical_buckets_state)]
+            
+            current_num = nums[index]
+            
+            for i in range(k):
+                if i > 0 and buckets[i] == buckets[i-1]:
                     continue
 
-                if current_sum + nums[i] > target_sum:
-                    continue
-
-                if i > 0 and nums[i] == nums[i-1] and not ((used_mask >> (i-1)) & 1):
-                    continue
-
-                if solve(bucket_idx, current_sum + nums[i], used_mask | (1 << i)):
-                    memo[(used_mask, current_sum)] = True
-                    return True
-
-            memo[(used_mask, current_sum)] = False
+                if buckets[i] + current_num <= target_sum_per_subset:
+                    buckets[i] += current_num
+                    
+                    if backtrack(index + 1):
+                        memo[(index, canonical_buckets_state)] = True
+                        return True
+                    
+                    buckets[i] -= current_num
+            
+            memo[(index, canonical_buckets_state)] = False
             return False
 
-        return solve(0, 0, 0)
+        return backtrack(0)
