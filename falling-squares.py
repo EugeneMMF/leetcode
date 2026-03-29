0f68aafa358c7003d9b1a5b6d4cc7c01a34bedf7
root <eugmwanmwan@gmail.com>
2026-03-29 06:03:08 +0000

    *** Added solution to problem  699 Falling Squares

--- falling-squares.py
+++ falling-squares.py
@@ -2,23 +2,22 @@ class Solution:
     def fallingSquares(self, positions: list[list[int]]) -> list[int]:
         landed_squares = []
         ans = []
-        current_overall_max_height = 0
+        max_overall_height = 0
 
         for left, side in positions:
             right = left + side
-            
-            current_square_base_y = 0 
-            
+            current_square_bottom_y = 0
+
             for prev_left, prev_right, prev_height in landed_squares:
                 if max(left, prev_left) < min(right, prev_right):
-                    current_square_base_y = max(current_square_base_y, prev_height)
+                    current_square_bottom_y = max(current_square_bottom_y, prev_height)
             
-            current_square_top_y = current_square_base_y + side
+            new_square_top_y = current_square_bottom_y + side
             
-            landed_squares.append((left, right, current_square_top_y))
+            landed_squares.append((left, right, new_square_top_y))
             
-            current_overall_max_height = max(current_overall_max_height, current_square_top_y)
+            max_overall_height = max(max_overall_height, new_square_top_y)
             
-            ans.append(current_overall_max_height)
+            ans.append(max_overall_height)
             
         return ans
