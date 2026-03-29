c7f0c15b070e58a6d4d710dbaee6c8a5fa953a93
root <eugmwanmwan@gmail.com>
2026-03-29 06:01:48 +0000

    *** Added solution to problem  697 Degree of an Array

--- degree-of-an-array.py
+++ degree-of-an-array.py
@@ -1,24 +1,26 @@
 class Solution:
-    def findShortestSubArray(self, nums: List[int]) -> int:
-        first_occurrence = {}
-        last_occurrence = {}
-        counts = {}
-
+    def findShortestSubArray(self, nums: list[int]) -> int:
+        num_info = {}
+        
         for i, num in enumerate(nums):
-            if num not in first_occurrence:
-                first_occurrence[num] = i
-            last_occurrence[num] = i
-            counts[num] = counts.get(num, 0) + 1
+            if num not in num_info:
+                num_info[num] = [1, i, i]
+            else:
+                num_info[num][0] += 1
+                num_info[num][2] = i
         
-        degree = 0
-        for count in counts.values():
-            degree = max(degree, count)
-        
+        max_degree = 0
+        for info in num_info.values():
+            max_degree = max(max_degree, info[0])
+            
         min_length = float('inf')
         
-        for num, count in counts.items():
-            if count == degree:
-                length = last_occurrence[num] - first_occurrence[num] + 1
-                min_length = min(min_length, length)
+        for num_value, info_list in num_info.items():
+            current_degree = info_list[0]
+            if current_degree == max_degree:
+                first_idx = info_list[1]
+                last_idx = info_list[2]
+                current_length = last_idx - first_idx + 1
+                min_length = min(min_length, current_length)
                 
         return min_length
