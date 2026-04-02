95776cb894375c07c3a7d1f132d6ab247c71ed9e
root <eugmwanmwan@gmail.com>
2026-04-02 06:02:03 +0000

    *** Added solution to problem  920 Uncommon Words from Two Sentences

---
+++ uncommon-words-from-two-sentences.py
@@ -0,0 +1,15 @@
+import collections
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> list[str]:
+        
+        all_words = s1.split() + s2.split()
+        
+        word_counts = collections.Counter(all_words)
+        
+        uncommon = []
+        for word, count in word_counts.items():
+            if count == 1:
+                uncommon.append(word)
+                
+        return uncommon
