d263d66baa58fabd4766af9e06d68bc208491e6e
root <eugmwanmwan@gmail.com>
2026-04-04 06:04:53 +0000

    *** Added solution to problem  1063 Best Sightseeing Pair

---
+++ best-sightseeing-pair.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: list[int]) -> int:
+        max_score = float('-inf')
+        max_val_plus_idx_so_far = values[0] + 0 
+
+        for j in range(1, len(values)):
+            current_score = max_val_plus_idx_so_far + (values[j] - j)
+            max_score = max(max_score, current_score)
+            
+            max_val_plus_idx_so_far = max(max_val_plus_idx_so_far, values[j] + j)
+            
+        return max_score
