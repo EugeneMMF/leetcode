a4e104681fea8bebb8cc22218cdef7389116953e
root <eugmwanmwan@gmail.com>
2026-04-05 06:07:59 +0000

    *** Added solution to problem  1229 Shortest Path with Alternating Colors

---
+++ shortest-path-with-alternating-colors.py
@@ -0,0 +1,51 @@
+
+from collections import deque
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        
+        adj_red = defaultdict(list)
+        for u, v in redEdges:
+            adj_red[u].append(v)
+        
+        adj_blue = defaultdict(list)
+        for u, v in blueEdges:
+            adj_blue[u].append(v)
+            
+        dist = [[float('inf')] * 2 for _ in range(n)]
+        
+        q = deque()
+        
+        dist[0][0] = 0
+        dist[0][1] = 0
+        q.append((0, 0))
+        q.append((0, 1))
+        
+        while q:
+            u, last_color = q.popleft()
+            current_distance = dist[u][last_color]
+            
+            next_color = 1 - last_color
+            
+            if next_color == 0:
+                for v in adj_red[u]:
+                    if current_distance + 1 < dist[v][0]:
+                        dist[v][0] = current_distance + 1
+                        q.append((v, 0))
+            else:
+                for v in adj_blue[u]:
+                    if current_distance + 1 < dist[v][1]:
+                        dist[v][1] = current_distance + 1
+                        q.append((v, 1))
+                        
+        answer = [-1] * n
+        for i in range(n):
+            min_dist = min(dist[i][0], dist[i][1])
+            if min_dist == float('inf'):
+                answer[i] = -1
+            else:
+                answer[i] = min_dist
+                
+        return answer
