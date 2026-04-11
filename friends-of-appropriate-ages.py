584e0778a78d31ab61720ae921eaf2cc10c5444d
root <eugmwanmwan@gmail.com>
2026-04-11 14:06:51 +0000

    *** Added solution to problem  825 Friends Of Appropriate Ages

--- friends-of-appropriate-ages.py
+++ friends-of-appropriate-ages.py
@@ -1,27 +1,20 @@
-
 class Solution:
-    def numFriendRequests(self, ages: list[int]) -> int:
-        counts = [0] * 121
+    def numFriendRequests(self, ages):
+        freq = [0] * 121
         for age in ages:
-            counts[age] += 1
-
-        total_requests = 0
-
-        for ageX in range(1, 121):
-            if counts[ageX] == 0:
+            freq[age] += 1
+        pref = [0] * 121
+        running = 0
+        for i in range(121):
+            running += freq[i]
+            pref[i] = running
+        total = 0
+        for a in range(15, 121):
+            if freq[a] == 0:
                 continue
-
-            for ageY in range(1, 121):
-                if counts[ageY] == 0:
-                    continue
-                
-                if (ageY > 0.5 * ageX + 7) and \
-                   (ageY <= ageX) and \
-                   not (ageY > 100 and ageX < 100):
-                    
-                    if ageX == ageY:
-                        total_requests += counts[ageX] * (counts[ageX] - 1)
-                    else:
-                        total_requests += counts[ageX] * counts[ageY]
-        
-        return total_requests
+            low = a // 2 + 8
+            if low > a:
+                continue
+            cnt = pref[a] - (pref[low - 1] if low > 0 else 0)
+            total += freq[a] * cnt - freq[a]
+        return total
