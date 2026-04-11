09f156972d098ce784378f3b09f226ac20e52c92
root <eugmwanmwan@gmail.com>
2026-04-11 14:06:00 +0000

    *** Added solution to problem  821 Shortest Distance to a Character

--- shortest-distance-to-a-character.py
+++ shortest-distance-to-a-character.py
@@ -1,19 +1,17 @@
+from typing import List
+
 class Solution:
-    def shortestToChar(self, s: str, c: str) -> list[int]:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
         n = len(s)
         ans = [0] * n
-
-        prev_c_idx = -n
-        for i in range(n):
-            if s[i] == c:
-                prev_c_idx = i
-            ans[i] = i - prev_c_idx
-
-        next_c_idx = 2 * n
+        prev = -10**9
+        for i, ch in enumerate(s):
+            if ch == c:
+                prev = i
+            ans[i] = i - prev
+        prev = 10**9
         for i in range(n - 1, -1, -1):
             if s[i] == c:
-                next_c_idx = i
-            ans[i] = min(ans[i], next_c_idx - i)
-            
+                prev = i
+            ans[i] = min(ans[i], prev - i)
         return ans
-
