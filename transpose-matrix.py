c1578e3d480966aa6bb1d6fc134e4f91f38fb120
root <eugmwanmwan@gmail.com>
2026-04-12 06:04:12 +0000

    *** Added solution to problem  867 Transpose Matrix

---
+++ transpose-matrix.py
@@ -0,0 +1,5 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        return [[matrix[i][j] for i in range(rows)] for j in range(cols)]
