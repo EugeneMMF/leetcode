61affaa304a0c650660e2c09b57a8f448459d205
root <eugmwanmwan@gmail.com>
2026-04-14 06:02:12 +0000

    *** Added solution to problem  943 Find the Shortest Superstring

--- find-the-shortest-superstring.py
+++ find-the-shortest-superstring.py
@@ -1,68 +1,35 @@
 class Solution:
-    def shortestSuperstring(self, words: list[str]) -> str:
+    def shortestSuperstring(self, words):
         n = len(words)
-
-        overlap = [[0] * n for _ in range(n)]
+        overlap = [[0]*n for _ in range(n)]
+        add = [[0]*n for _ in range(n)]
         for i in range(n):
             for j in range(n):
-                if i == j:
-                    continue
-                for k in range(min(len(words[i]), len(words[j])), 0, -1):
-                    if words[i][-k:] == words[j][:k]:
+                if i == j: continue
+                max_ol = min(len(words[i]), len(words[j]))
+                for k in range(max_ol, -1, -1):
+                    if words[i].endswith(words[j][:k]):
                         overlap[i][j] = k
                         break
-
-        dp = [[float('inf')] * n for _ in range(1 << n)]
-        
-        parent = [[-1] * n for _ in range(1 << n)]
-
+                add[i][j] = words[j][overlap[i][j]:]
+        dp = [[""]*n for _ in range(1<<n)]
         for i in range(n):
-            dp[1 << i][i] = len(words[i])
-
-        for mask in range(1, 1 << n):
+            dp[1<<i][i] = words[i]
+        for mask in range(1<<n):
             for i in range(n):
-                if not (mask & (1 << i)):
+                if not (mask & (1<<i)) or dp[mask][i]=="":
                     continue
-
-                prev_mask = mask ^ (1 << i)
-                
                 for j in range(n):
-                    if j == i:
+                    if mask & (1<<j):
                         continue
-                    if not (prev_mask & (1 << j)):
-                        continue
-
-                    current_length = dp[prev_mask][j] + len(words[i]) - overlap[j][i]
-
-                    if current_length < dp[mask][i]:
-                        dp[mask][i] = current_length
-                        parent[mask][i] = j
-
-        min_len = float('inf')
-        last_word_idx = -1
-        full_mask = (1 << n) - 1
-
+                    nxt = mask | (1<<j)
+                    cand = dp[mask][i] + add[i][j]
+                    if dp[nxt][j]=="" or len(cand) < len(dp[nxt][j]):
+                        dp[nxt][j] = cand
+        full = (1<<n)-1
+        ans = ""
         for i in range(n):
-            if dp[full_mask][i] < min_len:
-                min_len = dp[full_mask][i]
-                last_word_idx = i
-
-        path = []
-        current_mask = full_mask
-        current_word_idx = last_word_idx
-
-        while current_word_idx != -1:
-            path.append(current_word_idx)
-            prev_word_idx = parent[current_mask][current_word_idx]
-            current_mask ^= (1 << current_word_idx)
-            current_word_idx = prev_word_idx
-
-        path.reverse()
-
-        res = words[path[0]]
-        for k in range(1, n):
-            prev_idx = path[k-1]
-            curr_idx = path[k]
-            res += words[curr_idx][overlap[prev_idx][curr_idx]:]
-            
-        return res
+            cur = dp[full][i]
+            if cur and (ans=="" or len(cur) < len(ans)):
+                ans = cur
+        return ans
