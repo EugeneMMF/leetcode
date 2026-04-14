62b897522e004a5f111b830daac18ddd3dd666d1
root <eugmwanmwan@gmail.com>
2026-04-14 06:02:54 +0000

    *** Added solution to problem  947 Most Stones Removed with Same Row or Column

--- most-stones-removed-with-same-row-or-column.py
+++ most-stones-removed-with-same-row-or-column.py
@@ -1,35 +1,36 @@
+from typing import List
+
 class Solution:
-    def removeStones(self, stones: list[list[int]]) -> int:
-        n = len(stones)
-
-        parent = {}
-        num_disjoint_sets = 0
-
-        def find(i):
-            nonlocal num_disjoint_sets
-            if i not in parent:
-                parent[i] = i
-                num_disjoint_sets += 1
-                return i
-            if parent[i] == i:
-                return i
-            parent[i] = find(parent[i])
-            return parent[i]
-
-        def union(i, j):
-            nonlocal num_disjoint_sets
-            root_i = find(i)
-            root_j = find(j)
-            
-            if root_i != root_j:
-                parent[root_i] = root_j
-                num_disjoint_sets -= 1
-                return True
-            return False
-
-        offset = 20000 
-        
-        for x, y in stones:
-            union(x, y + offset)
-        
-        return n - num_disjoint_sets
+    def removeStones(self, stones: List[List[int]]) -> int:
+        parent={}
+        rank={}
+        def find(x):
+            while parent[x]!=x:
+                parent[x]=parent[parent[x]]
+                x=parent[x]
+            return x
+        def union(a,b):
+            ra, rb = find(a), find(b)
+            if ra==rb:
+                return
+            if rank[ra]<rank[rb]:
+                parent[ra]=rb
+            elif rank[ra]>rank[rb]:
+                parent[rb]=ra
+            else:
+                parent[rb]=ra
+                rank[ra]+=1
+        for x,y in stones:
+            row=('r',x)
+            col=('c',y)
+            if row not in parent:
+                parent[row]=row
+                rank[row]=0
+            if col not in parent:
+                parent[col]=col
+                rank[col]=0
+            union(row,col)
+        roots=set()
+        for node in parent:
+            roots.add(find(node))
+        return len(stones)-len(roots)
