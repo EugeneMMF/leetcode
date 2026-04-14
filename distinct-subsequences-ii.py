bafddbe6e4a558fc567eb9c110567fe4e93e8c65
root <eugmwanmwan@gmail.com>
2026-04-14 06:01:42 +0000

    *** Added solution to problem  940 Distinct Subsequences II

--- distinct-subsequences-ii.py
+++ distinct-subsequences-ii.py
@@ -1,20 +1,14 @@
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
-        MOD = 10**9 + 7
-        
-        dp = [0] * 26 
-        
-        total_distinct = 0
-        
-        for char_code in map(ord, s):
-            char_idx = char_code - ord('a')
-            
-            new_subsequences_ending_with_char = (total_distinct + 1) % MOD
-            
-            subtrahend = dp[char_idx]
-            
-            dp[char_idx] = new_subsequences_ending_with_char
-            
-            total_distinct = (total_distinct + new_subsequences_ending_with_char - subtrahend + MOD) % MOD
-            
-        return total_distinct
+        mod = 10**9 + 7
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        last = {}
+        for i in range(1, n + 1):
+            c = s[i - 1]
+            dp[i] = (dp[i - 1] * 2) % mod
+            if c in last:
+                dp[i] = (dp[i] - dp[last[c] - 1]) % mod
+            last[c] = i
+        return (dp[n] - 1) % mod
