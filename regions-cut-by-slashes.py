f2900646168c4443f22566ea14214b6b9444d86b
root <eugmwanmwan@gmail.com>
2026-04-14 06:05:27 +0000

    *** Added solution to problem  959 Regions Cut By Slashes

--- regions-cut-by-slashes.py
+++ regions-cut-by-slashes.py
@@ -1,31 +1,45 @@
 class Solution:
-    def regionsBySlashes(self, grid: list[str]) -> int:
+    def regionsBySlashes(self, grid):
         n = len(grid)
-        dot = [[0] * (3 * n) for _ in range(3 * n)]
-        for r in range(n):
-            for c in range(n):
-                if grid[r][c] == '/':
-                    dot[3 * r + 0][3 * c + 2] = 1
-                    dot[3 * r + 1][3 * c + 1] = 1
-                    dot[3 * r + 2][3 * c + 0] = 1
-                elif grid[r][c] == '\\':
-                    dot[3 * r + 0][3 * c + 0] = 1
-                    dot[3 * r + 1][3 * c + 1] = 1
-                    dot[3 * r + 2][3 * c + 2] = 1
-
-        count = 0
-        for r in range(3 * n):
-            for c in range(3 * n):
-                if dot[r][c] == 0:
-                    count += 1
-                    stack = [(r, c)]
-                    dot[r][c] = 1
-                    while stack:
-                        row, col = stack.pop()
-                        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                            nr, nc = row + dr, col + dc
-                            if 0 <= nr < 3 * n and 0 <= nc < 3 * n and dot[nr][nc] == 0:
-                                dot[nr][nc] = 1
-                                stack.append((nr, nc))
-        return count
-
+        parent = list(range(n * n * 4))
+        rank = [0] * (n * n * 4)
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        def union(x, y):
+            rx, ry = find(x), find(y)
+            if rx == ry:
+                return
+            if rank[rx] < rank[ry]:
+                parent[rx] = ry
+            elif rank[rx] > rank[ry]:
+                parent[ry] = rx
+            else:
+                parent[ry] = rx
+                rank[rx] += 1
+        for i in range(n):
+            for j in range(n):
+                base = (i * n + j) * 4
+                c = grid[i][j]
+                if c == ' ':
+                    union(base, base + 1)
+                    union(base + 1, base + 2)
+                    union(base + 2, base + 3)
+                elif c == '/':
+                    union(base, base + 3)
+                    union(base + 1, base + 2)
+                else:
+                    union(base, base + 1)
+                    union(base + 2, base + 3)
+                if i + 1 < n:
+                    bottom = base + 2
+                    down_top = ((i + 1) * n + j) * 4
+                    union(bottom, down_top)
+                if j + 1 < n:
+                    right = base + 1
+                    left = ((i) * n + j + 1) * 4 + 3
+                    union(right, left)
+        regions = sum(1 for i in range(len(parent)) if find(i) == i)
+        return regions
