a65a40b5cd6e347c248a81fb0fa32598f7c6e7ae
root <eugmwanmwan@gmail.com>
2026-04-15 06:07:29 +0000

    *** Added solution to problem  1005 Maximize Sum Of Array After K Negations

--- maximize-sum-of-array-after-k-negations.py
+++ maximize-sum-of-array-after-k-negations.py
@@ -1,11 +1,13 @@
-import heapq
-
 class Solution:
-    def largestSumAfterKNegations(self, nums: list[int], k: int) -> int:
-        heapq.heapify(nums)
-
-        for _ in range(k):
-            smallest = heapq.heappop(nums)
-            heapq.heappush(nums, -smallest)
-        
-        return sum(nums)
+    def largestSumAfterKNegations(self, nums, k):
+        nums.sort()
+        i = 0
+        n = len(nums)
+        while i < n and k > 0 and nums[i] < 0:
+            nums[i] = -nums[i]
+            k -= 1
+            i += 1
+        total = sum(nums)
+        if k % 2 == 1:
+            total -= 2 * min(nums)
+        return total
