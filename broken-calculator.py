fdfe66be9d2e23d6f1e711e860faf91c877deab4
root <eugmwanmwan@gmail.com>
2026-04-15 06:04:44 +0000

    *** Added solution to problem  991 Broken Calculator

---
+++ broken-calculator.py
@@ -0,0 +1,10 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        ops = 0
+        while target > startValue:
+            ops += 1
+            if target % 2:
+                target += 1
+            else:
+                target //= 2
+        return ops + (startValue - target)
