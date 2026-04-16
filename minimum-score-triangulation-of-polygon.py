dbe3b87925439a166d649108fe4187a7e3613bef
root <eugmwanmwan@gmail.com>
2026-04-16 06:01:34 +0000

    *** Added solution to problem  1039 Minimum Score Triangulation of Polygon

---
+++ minimum-score-triangulation-of-polygon.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+        dp = [[0] * n for _ in range(n)]
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                best = 10 ** 9
+                for k in range(i + 1, j):
+                    cost = dp[i][k] + dp[k][j] + values[i] * values[k] * values[j]
+                    if cost < best:
+                        best = cost
+                dp[i][j] = best
+        return dp[0][n - 1]
