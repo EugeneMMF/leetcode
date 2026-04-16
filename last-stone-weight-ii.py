9fc96e9534ef9141d65b796e260e5bd4fc97ea4f
root <eugmwanmwan@gmail.com>
2026-04-16 15:45:43 +0000

    *** Added solution to problem  1049 Last Stone Weight II

---
+++ last-stone-weight-ii.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        total = sum(stones)
+        target = total // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for w in stones:
+            for s in range(target, w - 1, -1):
+                dp[s] = dp[s] or dp[s - w]
+        for s in range(target, -1, -1):
+            if dp[s]:
+                return total - 2 * s
+        return 0
