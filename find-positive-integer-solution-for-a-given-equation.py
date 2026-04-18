7dbaaa8ceaf4a8b94bccfe419f8b0e95a7fbe751
root <eugmwanmwan@gmail.com>
2026-04-18 06:05:52 +0000

    *** Added solution to problem  1237 Find Positive Integer Solution for a Given Equation

--- find-positive-integer-solution-for-a-given-equation.py
+++ find-positive-integer-solution-for-a-given-equation.py
@@ -1,28 +1,29 @@
-# class CustomFunction:
-#    def f(self, x, y):
-#        """
-#        :type x: int
-#        :type y: int
-#        :rtype: int
-#        """
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+
+from typing import List
 
 class Solution:
-    def findSolution(self, customfunction: 'CustomFunction', z: int) -> list[list[int]]:
-        results = []
-        
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        res = []
         x = 1
-        y = 1000 
-        
+        y = 1000
         while x <= 1000 and y >= 1:
-            current_f_val = customfunction.f(x, y)
-            
-            if current_f_val == z:
-                results.append([x, y])
+            v = customfunction.f(x, y)
+            if v == z:
+                res.append([x, y])
                 x += 1
                 y -= 1
-            elif current_f_val < z:
+            elif v < z:
                 x += 1
             else:
                 y -= 1
-                
-        return results
+        return res
