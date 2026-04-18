1f046f899bb039a4d7feae4a467c8d1a6c495eb0
root <eugmwanmwan@gmail.com>
2026-04-18 06:05:15 +0000

    *** Added solution to problem  1233 Remove Sub-Folders from the Filesystem

--- remove-sub-folders-from-the-filesystem.py
+++ remove-sub-folders-from-the-filesystem.py
@@ -1,15 +1,8 @@
 class Solution:
-    def removeSubfolders(self, folder: list[str]) -> list[str]:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
         folder.sort()
-
-        result = []
-
-        for current_folder in folder:
-            if not result:
-                result.append(current_folder)
-            else:
-                last_root_folder = result[-1]
-                if not current_folder.startswith(last_root_folder + "/"):
-                    result.append(current_folder)
-        
-        return result
+        res = []
+        for f in folder:
+            if not res or not f.startswith(res[-1] + '/'):
+                res.append(f)
+        return res
