8cb2d51187c9bc787f7cd799f88f688b930c3e33
root <eugmwanmwan@gmail.com>
2026-04-18 06:06:31 +0000

    *** Added solution to problem  1247 Minimum Swaps to Make Strings Equal

--- minimum-swaps-to-make-strings-equal.py
+++ minimum-swaps-to-make-strings-equal.py
@@ -1,25 +1,12 @@
 class Solution:
     def minimumSwap(self, s1: str, s2: str) -> int:
-        count_xy = 0
-        count_yx = 0
-
-        for i in range(len(s1)):
-            if s1[i] == s2[i]:
-                continue
-            elif s1[i] == 'x' and s2[i] == 'y':
-                count_xy += 1
-            else:
-                count_yx += 1
-        
-        if (count_xy + count_yx) % 2 != 0:
+        a = b = 0
+        for c1, c2 in zip(s1, s2):
+            if c1 != c2:
+                if c1 == 'x':
+                    a += 1
+                else:
+                    b += 1
+        if (a + b) % 2:
             return -1
-        
-        swaps = 0
-        
-        swaps += count_xy // 2
-        swaps += count_yx // 2
-        
-        if count_xy % 2 == 1:
-            swaps += 2
-            
-        return swaps
+        return a // 2 + b // 2 + (2 if a % 2 else 0)
