d8bfed0a5e0f48b216784c597ad86ecaa7fd46d8
root <eugmwanmwan@gmail.com>
2026-04-18 06:01:33 +0000

    *** Added solution to problem  1204 Last Person to Fit in the Bus

---
+++ last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT person_name
+FROM (
+    SELECT person_name, turn, SUM(weight) OVER (ORDER BY turn) AS cum_weight
+    FROM Queue
+) AS t
+WHERE cum_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
