54fd4c28950645d826f3b882517d79506ba9fe9b
root <eugmwanmwan@gmail.com>
2026-04-19 06:01:29 +0000

    *** Added solution to problem  1304 Find N Unique Integers Sum up to Zero

---
+++ find-n-unique-integers-sum-up-to-zero.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+        half = n // 2
+        for i in range(1, half + 1):
+            res.append(i)
+            res.append(-i)
+        if n % 2:
+            res.append(0)
+        return res[:n]
