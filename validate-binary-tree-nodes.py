97c83015e93a094a955b6103e55a7ddd4539a2e7
root <eugmwanmwan@gmail.com>
2026-04-20 06:05:36 +0000

    *** Added solution to problem  1361 Validate Binary Tree Nodes

--- validate-binary-tree-nodes.py
+++ validate-binary-tree-nodes.py
@@ -1,49 +1,32 @@
-import collections
-
 class Solution:
-    def validateBinaryTreeNodes(self, n: int, leftChild: list[int], rightChild: list[int]) -> bool:
-        in_degree = [0] * n
-
+    def validateBinaryTreeNodes(self, n, leftChild, rightChild):
+        indeg = [0] * n
         for i in range(n):
-            if leftChild[i] != -1:
-                in_degree[leftChild[i]] += 1
-            if rightChild[i] != -1:
-                in_degree[rightChild[i]] += 1
-        
-        root = -1
-        for i in range(n):
-            if in_degree[i] == 0:
-                if root != -1:
+            l = leftChild[i]
+            r = rightChild[i]
+            if l != -1:
+                indeg[l] += 1
+                if indeg[l] > 1:
                     return False
-                root = i
-            elif in_degree[i] > 1:
-                return False
-        
-        if root == -1:
+            if r != -1:
+                indeg[r] += 1
+                if indeg[r] > 1:
+                    return False
+        roots = [i for i, d in enumerate(indeg) if d == 0]
+        if len(roots) != 1:
             return False
-            
-        q = collections.deque([root])
-        visited_count = 0
-        
-        visited = {root}
-
-        while q:
-            node = q.popleft()
-            visited_count += 1
-            
-            left_child_node = leftChild[node]
-            if left_child_node != -1:
-                if left_child_node in visited:
-                    return False
-                visited.add(left_child_node)
-                q.append(left_child_node)
-            
-            right_child_node = rightChild[node]
-            if right_child_node != -1:
-                if right_child_node in visited:
-                    return False
-                visited.add(right_child_node)
-                q.append(right_child_node)
-        
-        return visited_count == n
-
+        root = roots[0]
+        stack = [root]
+        visited = set()
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                return False
+            visited.add(node)
+            l = leftChild[node]
+            r = rightChild[node]
+            if l != -1:
+                stack.append(l)
+            if r != -1:
+                stack.append(r)
+        return len(visited) == n
