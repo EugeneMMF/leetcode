36193b07fa1ca67bda69a7997df6fb4620bbeb6b
root <eugmwanmwan@gmail.com>
2026-04-20 06:05:55 +0000

    *** Added solution to problem  1363 Largest Multiple of Three

--- largest-multiple-of-three.py
+++ largest-multiple-of-three.py
@@ -1,37 +1,42 @@
+from typing import List
+
 class Solution:
-    def largestMultipleOfThree(self, digits: list[int]) -> str:
-        digits.sort()
-
-        total_sum = sum(digits)
-        mod1 = [d for d in digits if d % 3 == 1]
-        mod2 = [d for d in digits if d % 3 == 2]
-
-        digits_to_remove = []
-
-        if total_sum % 3 == 1:
-            if len(mod1) >= 1:
-                digits_to_remove.append(mod1[0])
-            elif len(mod2) >= 2:
-                digits_to_remove.append(mod2[0])
-                digits_to_remove.append(mod2[1])
-        elif total_sum % 3 == 2:
-            if len(mod2) >= 1:
-                digits_to_remove.append(mod2[0])
-            elif len(mod1) >= 2:
-                digits_to_remove.append(mod1[0])
-                digits_to_remove.append(mod1[1])
-
-        temp_digits = list(digits)
-        for d_rem in digits_to_remove:
-            temp_digits.remove(d_rem)
-        final_digits = temp_digits
-
-        final_digits.sort(reverse=True)
-
-        if not final_digits:
+    def largestMultipleOfThree(self, digits: List[int]) -> str:
+        cnt = [0] * 10
+        total = 0
+        for d in digits:
+            cnt[d] += 1
+            total += d
+        mod = total % 3
+        def remove_one(rem):
+            for i in range(1, 10):
+                if i % 3 == rem and cnt[i]:
+                    cnt[i] -= 1
+                    return True
+            return False
+        def remove_two(rem):
+            removed = 0
+            for i in range(1, 10):
+                while i % 3 == rem and cnt[i] and removed < 2:
+                    cnt[i] -= 1
+                    removed += 1
+                    if removed == 2:
+                        return True
+            return False
+        if mod == 1:
+            if not remove_one(1):
+                if not remove_two(2):
+                    return ""
+        elif mod == 2:
+            if not remove_one(2):
+                if not remove_two(1):
+                    return ""
+        res = []
+        for i in range(9, -1, -1):
+            res.append(str(i) * cnt[i])
+        ans = ''.join(res)
+        if not ans:
             return ""
-        if final_digits[0] == 0:
+        if ans[0] == '0':
             return "0"
-
-        return "".join(map(str, final_digits))
-
+        return ans
