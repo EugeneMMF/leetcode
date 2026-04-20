bce55bf95cddd2d4fede55edaa255642db61e014
root <eugmwanmwan@gmail.com>
2026-04-20 06:06:23 +0000

    *** Added solution to problem  1367 Linked List in Binary Tree

--- linked-list-in-binary-tree.py
+++ linked-list-in-binary-tree.py
@@ -1,35 +1,26 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSubPath(self, head: ListNode, root: TreeNode) -> bool:
-        def dfs(node, current_list_node):
+    def isSubPath(self, head, root):
+        def match(node, lst):
             if not node:
                 return False
-            
-            if node.val == current_list_node.val:
-                if not current_list_node.next:
-                    return True
-                return dfs(node.left, current_list_node.next) or dfs(node.right, current_list_node.next)
-            else:
+            if node.val != lst.val:
                 return False
-
-        def check(node, list_head):
-            if not node:
-                return False
-            
-            if dfs(node, list_head):
+            if not lst.next:
                 return True
-            
-            return check(node.left, list_head) or check(node.right, list_head)
-        
-        return check(root, head)
-
+            return match(node.left, lst.next) or match(node.right, lst.next)
+        if not root:
+            return False
+        if match(root, head):
+            return True
+        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
