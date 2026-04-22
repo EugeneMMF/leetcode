26c62b5bdd6372926a07c778ad90473d8c7108e8
root <eugmwanmwan@gmail.com>
2026-04-22 06:05:54 +0000

    *** Added solution to problem  1450 Number of Students Doing Homework at a Given Time

---
+++ number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(1 for s, e in zip(startTime, endTime) if s <= queryTime <= e)
