3b647a5bdabeb01c11d393ea8b7123c3a8e2b959
root <eugmwanmwan@gmail.com>
2026-04-22 06:03:42 +0000

    *** Added solution to problem  1436 Destination City

---
+++ destination-city.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        starts = set()
+        for a, b in paths:
+            starts.add(a)
+        for a, b in paths:
+            if b not in starts:
+                return b
