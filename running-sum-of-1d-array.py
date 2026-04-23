11289658006912b0a8f987ce77e1634f40a2d0fc
root <eugmwanmwan@gmail.com>
2026-04-23 06:04:09 +0000

    *** Added solution to problem  1480 Running Sum of 1d Array

---
+++ running-sum-of-1d-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        res = []
+        for x in nums:
+            total += x
+            res.append(total)
+        return res
