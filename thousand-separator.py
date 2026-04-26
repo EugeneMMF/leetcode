e9eddf68fa509e50343706d450b8852cc468a36a
root <eugmwanmwan@gmail.com>
2026-04-26 20:15:41 +0000

    *** Added solution to problem  1556 Thousand Separator

---
+++ thousand-separator.py
@@ -0,0 +1,8 @@
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+        s = str(n)
+        parts = []
+        while s:
+            parts.append(s[-3:])
+            s = s[:-3]
+        return '.'.join(reversed(parts))
