13bb3da20151bae8258914b9c66a4ee904e48c6a
root <eugmwanmwan@gmail.com>
2026-04-26 20:17:02 +0000

    *** Added solution to problem  1562 Find Latest Group of Size M

---
+++ find-latest-group-of-size-m.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findLatestStep(self, arr, m):
+        n = len(arr)
+        if m == n:
+            return n
+        length = [0] * (n + 2)
+        count = [0] * (n + 1)
+        res = -1
+        for step, pos in enumerate(arr, 1):
+            left = length[pos - 1]
+            right = length[pos + 1]
+            new_len = left + right + 1
+            length[pos - left] = new_len
+            length[pos + right] = new_len
+            count[left] -= 1
+            count[right] -= 1
+            count[new_len] += 1
+            if count[m] > 0:
+                res = step
+        return res
