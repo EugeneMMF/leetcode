4375fcb0159787e3f848d3f61bdac82c55cd6583
root <eugmwanmwan@gmail.com>
2026-04-26 20:14:57 +0000

    *** Added solution to problem  1550 Three Consecutive Odds

---
+++ three-consecutive-odds.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0
+        for x in arr:
+            if x % 2:
+                cnt += 1
+                if cnt == 3:
+                    return True
+            else:
+                cnt = 0
+        return False
