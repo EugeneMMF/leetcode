3eb6f9fe84b84ad285fdcedc5251cdc6c8d11df2
root <eugmwanmwan@gmail.com>
2026-04-27 11:29:41 +0000

    *** Added solution to problem  1591 Strange Printer II

--- strange-printer-ii.py
+++ strange-printer-ii.py
@@ -1,50 +1,38 @@
 class Solution:
-    def isPrintable(self, targetGrid: list[list[int]]) -> bool:
-        m, n = len(targetGrid), len(targetGrid[0])
-        colors = set()
-        for r in range(m):
-            for c in range(n):
-                colors.add(targetGrid[r][c])
-
-        adj = {color: set() for color in colors}
-        pos = {color: [] for color in colors}
-
-        for r in range(m):
-            for c in range(n):
-                pos[targetGrid[r][c]].append((r, c))
-
-        for color in colors:
-            min_r = min(r for r, c in pos[color])
-            max_r = max(r for r, c in pos[color])
-            min_c = min(c for r, c in pos[color])
-            max_c = max(c for r, c in pos[color])
-
-            for r in range(min_r, max_r + 1):
-                for c in range(min_c, max_c + 1):
-                    if targetGrid[r][c] != color:
-                        adj[targetGrid[r][c]].add(color)
-
-        visited = set()
-        recursion_stack = set()
-
-        def dfs(color):
-            visited.add(color)
-            recursion_stack.add(color)
-
-            for neighbor in adj[color]:
-                if neighbor not in visited:
-                    if not dfs(neighbor):
-                        return False
-                elif neighbor in recursion_stack:
-                    return False
-
-            recursion_stack.remove(color)
-            return True
-
-        for color in colors:
-            if color not in visited:
-                if not dfs(color):
-                    return False
-        
-        return True
-
+    def isPrintable(self, targetGrid):
+        m=len(targetGrid); n=len(targetGrid[0])
+        max_color=60
+        mins=[[m, n] for _ in range(max_color+1)]
+        maxs=[[-1, -1] for _ in range(max_color+1)]
+        present=set()
+        for i in range(m):
+            for j in range(n):
+                c=targetGrid[i][j]
+                present.add(c)
+                if i<mins[c][0]: mins[c][0]=i
+                if j<mins[c][1]: mins[c][1]=j
+                if i>maxs[c][0]: maxs[c][0]=i
+                if j>maxs[c][1]: maxs[c][1]=j
+        indeg={c:0 for c in present}
+        adj={c:set() for c in present}
+        for c in present:
+            r1,r2=mins[c][0],maxs[c][0]
+            c1,c2=mins[c][1],maxs[c][1]
+            for i in range(r1, r2+1):
+                for j in range(c1, c2+1):
+                    d=targetGrid[i][j]
+                    if d!=c:
+                        if d not in adj[c]:
+                            adj[c].add(d)
+                            indeg[d]+=1
+        from collections import deque
+        q=deque([c for c in present if indeg[c]==0])
+        cnt=0
+        while q:
+            cur=q.popleft()
+            cnt+=1
+            for nb in adj[cur]:
+                indeg[nb]-=1
+                if indeg[nb]==0:
+                    q.append(nb)
+        return cnt==len(present)
