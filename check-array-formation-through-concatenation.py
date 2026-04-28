124f6c88c8f21eb78b74cc86db47c944c2c7e9f8
root <eugmwanmwan@gmail.com>
2026-04-28 06:03:39 +0000

    *** Added solution to problem  1640 Check Array Formation Through Concatenation

---
+++ check-array-formation-through-concatenation.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        mp = {p[0]: p for p in pieces}
+        i = 0
+        n = len(arr)
+        while i < n:
+            if arr[i] not in mp:
+                return False
+            piece = mp[arr[i]]
+            for num in piece:
+                if i >= n or arr[i] != num:
+                    return False
+                i += 1
+        return True
