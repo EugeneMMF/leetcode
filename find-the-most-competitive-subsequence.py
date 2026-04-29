5cee092bd3564a368ac7cbd6947e6341d3cf4eac
root <eugmwanmwan@gmail.com>
2026-04-29 06:03:22 +0000

    *** Added solution to problem  1673 Find the Most Competitive Subsequence

---
+++ find-the-most-competitive-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mostCompetitive(self, nums, k):
+        stack = []
+        n = len(nums)
+        for i, num in enumerate(nums):
+            while stack and stack[-1] > num and len(stack) - 1 + (n - i) >= k:
+                stack.pop()
+            if len(stack) < k:
+                stack.append(num)
+        return stack
