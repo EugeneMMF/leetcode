a8a56a3a57403522b345cfbe74bfb988a6f5b363
root <eugmwanmwan@gmail.com>
2026-05-08 06:06:34 +0000

    *** Added solution to problem  1773 Count Items Matching a Rule

---
+++ count-items-matching-a-rule.py
@@ -0,0 +1,11 @@
+import typing
+from typing import List
+
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        idx = {"type": 0, "color": 1, "name": 2}[ruleKey]
+        count = 0
+        for item in items:
+            if item[idx] == ruleValue:
+                count += 1
+        return count
