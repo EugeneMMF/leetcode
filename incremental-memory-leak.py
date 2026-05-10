638385e259c696f2f1f44c957a9109b9f5ecd7c5
root <eugmwanmwan@gmail.com>
2026-05-10 06:09:32 +0000

    *** Added solution to problem  1860 Incremental Memory Leak

---
+++ incremental-memory-leak.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def memLeak(self, memory1: int, memory2: int) -> List[int]:
+        i = 1
+        while True:
+            if memory1 >= memory2:
+                if memory1 < i:
+                    break
+                memory1 -= i
+            else:
+                if memory2 < i:
+                    break
+                memory2 -= i
+            i += 1
+        return [i, memory1, memory2]
