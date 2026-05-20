00983cc98a44d06c025c27a502e2ee2106cb692f
root <eugmwanmwan@gmail.com>
2026-05-20 06:01:19 +0000

    *** Added solution to problem  2148 Count Elements With Strictly Smaller and Greater Elements

---
+++ count-elements-with-strictly-smaller-and-greater-elements.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def countElements(self, nums: List[int]) -> int:
+        min_val = min(nums)
+        max_val = max(nums)
+        return sum(1 for x in nums if min_val < x < max_val)
