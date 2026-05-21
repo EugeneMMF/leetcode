11e92e0cb65be308ce3754ab41ef72eba53ec13b
root <eugmwanmwan@gmail.com>
2026-05-21 06:03:16 +0000

    *** Added solution to problem  2191 Sort the Jumbled Numbers

---
+++ sort-the-jumbled-numbers.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        def mapped_value(num: int) -> int:
+            return int(''.join(str(mapping[int(c)]) for c in str(num)))
+        return sorted(nums, key=mapped_value)
