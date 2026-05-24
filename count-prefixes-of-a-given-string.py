15d48fc96d5fcc73beb3dacd9d3b3df3e39d81c8
root <eugmwanmwan@gmail.com>
2026-05-24 06:05:25 +0000

    *** Added solution to problem  2255 Count Prefixes of a Given String

---
+++ count-prefixes-of-a-given-string.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        count = 0
+        for w in words:
+            if s.startswith(w):
+                count += 1
+        return count
