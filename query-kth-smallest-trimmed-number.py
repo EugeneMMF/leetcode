687e18a34cb3861a0ec8b186a28935dd72ed15b8
root <eugmwanmwan@gmail.com>
2026-06-01 06:02:33 +0000

    *** Added solution to problem  2343 Query Kth Smallest Trimmed Number

---
+++ query-kth-smallest-trimmed-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+        answer = []
+        for k, trim in queries:
+            trimmed = [(num[-trim:], i) for i, num in enumerate(nums)]
+            trimmed.sort(key=lambda x: (x[0], x[1]))
+            answer.append(trimmed[k-1][1])
+        return answer
