2efc4fc321ed4577fd3a301c052b3ed02dc91e32
root <eugmwanmwan@gmail.com>
2026-06-01 06:03:04 +0000

    *** Added solution to problem  2347 Best Poker Hand

---
+++ best-poker-hand.py
@@ -0,0 +1,11 @@
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        if len(set(suits)) == 1:
+            return "Flush"
+        from collections import Counter
+        cnt = Counter(ranks)
+        if any(v >= 3 for v in cnt.values()):
+            return "Three of a Kind"
+        if any(v >= 2 for v in cnt.values()):
+            return "Pair"
+        return "High Card"
