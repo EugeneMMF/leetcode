eb52d979d958343ef3eab4361895e9f6a10bdd78
root <eugmwanmwan@gmail.com>
2026-06-03 06:08:32 +0000

    *** Added solution to problem  2442 Count Number of Distinct Integers After Reverse Operations

---
+++ count-number-of-distinct-integers-after-reverse-operations.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def countDistinctIntegers(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            seen.add(num)
+            seen.add(int(str(num)[::-1]))
+        return len(seen)
