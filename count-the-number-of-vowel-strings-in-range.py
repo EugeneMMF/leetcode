1e5a76d0e633e786163813630757db660c1b1129
root <eugmwanmwan@gmail.com>
2026-06-11 21:21:13 +0000

    *** Added solution to problem  2586 Count the Number of Vowel Strings in Range

---
+++ count-the-number-of-vowel-strings-in-range.py
@@ -0,0 +1,8 @@
+class Solution:
+    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+        vowels = set('aeiou')
+        count = 0
+        for w in words[left:right+1]:
+            if w[0] in vowels and w[-1] in vowels:
+                count += 1
+        return count
