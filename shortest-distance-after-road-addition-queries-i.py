90f7bce974947848df03a55ac79e8d38557ee652
root <eugmwanmwan@gmail.com>
2026-07-23 08:14:23 +0000

    *** Added solution to problem  3243 Shortest Distance After Road Addition Queries I

--- shortest-distance-after-road-addition-queries-i.py
+++ shortest-distance-after-road-addition-queries-i.py
@@ -1,36 +1,24 @@
-  
+from collections import deque
+from typing import List
+
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        from collections import defaultdict
-        import heapq
-        
-        graph = defaultdict(list)
+        adj = [[] for _ in range(n)]
         for i in range(n - 1):
-            graph[i].append(i + 1)
-        
-        result = []
-        
-        def dijkstra():
-            min_heap = [(0, 0)]
-            distances = {i: float('inf') for i in range(n)}
-            distances[0] = 0
-            
-            while min_heap:
-                current_distance, current_node = heapq.heappop(min_heap)
-                if current_distance > distances[current_node]:
-                    continue
-                for neighbor in graph[current_node]:
-                    distance = current_distance + 1
-                    if distance < distances[neighbor]:
-                        distances[neighbor] = distance
-                        heapq.heappush(min_heap, (distance, neighbor))
-            return distances[n - 1]
-        
+            adj[i].append(i + 1)
+        res = []
         for u, v in queries:
-            graph[u].append(v)
-            shortest_path_length = dijkstra()
-            result.append(shortest_path_length)
-        
-        return result
-
-    
+            adj[u].append(v)
+            dist = [-1] * n
+            dq = deque([0])
+            dist[0] = 0
+            while dq:
+                x = dq.popleft()
+                if x == n - 1:
+                    break
+                for y in adj[x]:
+                    if dist[y] == -1:
+                        dist[y] = dist[x] + 1
+                        dq.append(y)
+            res.append(dist[n - 1])
+        return res
