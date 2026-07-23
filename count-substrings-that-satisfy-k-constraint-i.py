9b2aa4cb8b51a09015f18d3c0b989284b67a1ac9
root <eugmwanmwan@gmail.com>
2026-07-23 09:14:48 +0000

    *** Added solution to problem  3258 Count Substrings That Satisfy K-Constraint I

--- count-substrings-that-satisfy-k-constraint-i.py
+++ count-substrings-that-satisfy-k-constraint-i.py
@@ -1,18 +1,14 @@
-
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
-        count = 0
         n = len(s)
+        pref0 = [0] * (n + 1)
+        for i, ch in enumerate(s):
+            pref0[i + 1] = pref0[i] + (1 if ch == '0' else 0)
+        ans = 0
         for i in range(n):
-            count0 = count1 = 0
             for j in range(i, n):
-                if s[j] == '0':
-                    count0 += 1
-                else:
-                    count1 += 1
-                if count0 > k and count1 > k:
-                    break
-                count += 1
-        return count
-
-        
+                zeros = pref0[j + 1] - pref0[i]
+                ones = (j - i + 1) - zeros
+                if zeros <= k or ones <= k:
+                    ans += 1
+        return ans
