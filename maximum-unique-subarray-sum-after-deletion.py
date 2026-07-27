fa6fc3991f5b353e29e9bf28cdc212db82187e2c
root <eugmwanmwan@gmail.com>
2026-07-27 06:14:03 +0000

    *** Added solution to problem  3487 Maximum Unique Subarray Sum After Deletion

---
+++ maximum-unique-subarray-sum-after-deletion.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        positives = {x for x in nums if x > 0}
+        if positives:
+            return sum(positives)
+        return max(nums)
